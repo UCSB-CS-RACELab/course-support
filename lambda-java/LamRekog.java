4407791c54760ff688b981e8b8846d9598c5a07d
Chandra <ckrintz@ucsb.edu>
2019-06-07 12:21:20 -0700

    code clean up and documentation

--- lambda-java/LamRekog.java
+++ lambda-java/LamRekog.java
@@ -12,7 +12,17 @@ import com.amazonaws.services.rekognition.model.*;
 
 public class LamRekog {
 
+    /* Use a logger for multi-line CloudWatch logging when in AWS Lambda.
+     * If there is none, we  will default to System.err.println
+     */
     LambdaLogger logger = null;
+
+    /* Choose a Region in which to execute the Rekognition service
+     * see https://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/regions/Regions.html
+     */
+    final Regions preferred_region = Regions.US_WEST_2;
+
+    //Entry point for AWS Lambda invocation
     public void handler(S3Event event, Context context) {
 	/* Invokes the function in Lambda 
            (triggered by an S3 update (source event)) 
@@ -27,24 +37,36 @@ public class LamRekog {
             }         
     
         } catch (Exception e) {
-            logger.log("Error: " + e.getMessage());
-            throw new RuntimeException (e);
+            if (logger != null) {
+                logger.log("Error: " + e.getMessage());
+                logger.log(e.toString());
+	    } else {
+                System.err.println("Error: " + e.getMessage());
+                System.err.println(e);
+            }
         }
 
     }
 
     void detectLabels(String bkt, String fname) {
-        /*request labels from image file named filename in S3 bucket bkt
-          use 90% confidence limit and top 10 labels
+        /* Request labels from image file named filename in S3 bucket bkt
+         * use 90% confidence limit and top 10 labels.
+         * This method is invoked by main (local/test) 
+   	 * and by the handler (AWS Lambda).
          */
 
-        //setup the AWS credentials (from ~/.aws/credentials)
-        AWSCredentials credentials = new ProfileCredentialsProvider("racelab").getCredentials();
-        //create a client connected to Rekognition service
-        AmazonRekognition client = new AmazonRekognitionClient(credentials).withRegion(RegionUtils.getRegion("us-west-2"));
+        /* Create a client connected to Rekognition service.
+	 * Your credentials MUST be set in enviroment variables where
+	 * the program runs.  The variable names are AWS_ACCESS_KEY_ID
+         * and AWS_SECRET_ACCESS_KEY.  These are set for you in 
+	 * AWS Lambda, but if you run this locally, you need to set them.
+         * export VARNAME=variable_value or use this on Windows:
+         * https://stackoverflow.com/questions/44272416/how-to-add-a-folder-to-path-environment-variable-in-windows-10-with-screensho
+         */
+        AmazonRekognition client = AmazonRekognitionClientBuilder.standard().withRegion(preferred_region).build();
 
-        //pass in the bucket (bkt) and file name (fname) to Rekognition
-        //which returns the labels and confidence for lables with 
+        //pass in the bucket (bkt) and file name (fname) to Rekognition.
+        //Doing so returns the labels and confidence for lables with 
 	//confidence higher than 90%
         DetectLabelsRequest request = new DetectLabelsRequest()
             .withImage(new Image().withS3Object(new S3Object()
@@ -69,12 +91,17 @@ public class LamRekog {
         }
     }
 
+    //Entry point for command line invocation (local testing)
     public static void main(String[] args){
-	/* Invokes the function from main for local testing */
+
+	//create an instance of the encapsulating class
         LamRekog obj = new LamRekog();
+
+	//set the name of the file and the bucket in S3 for Rekognition
+        String s3Key = "cjkltestfolder/badger1.jpg";
+        String s3Bucket = "cjkltestbkt";
+
         try{ 
-            String s3Key = "cjkltestfolder/badger1.jpg";
-            String s3Bucket = "cjkltestbkt";
             obj.detectLabels(s3Bucket, s3Key);
         } catch (Exception e) {
             if (obj != null && obj.logger != null) {
@@ -82,7 +109,6 @@ public class LamRekog {
 	    } else {
 		System.err.println("Error: " + e.getMessage());
             }
-            throw new RuntimeException (e);
         }
 
     }
